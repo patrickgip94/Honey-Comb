3cac1eff28ac8071567cd1232b7f715bf672a0f8
Patrick Gip <gippatrick@gmail.com>
2022-11-30 22:37:51 -0800

    Query Books added #4

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,12 +1,54 @@
-import { StyleSheet } from 'react-native';
+import { ActivityIndicator, StyleSheet } from "react-native";
+import { Text, View } from "../components/Themed";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
 
-import { Text, View } from '../components/Themed';
-
+const query = gql`
+  query SearchBooks($q: String) {
+    googleBooksSearch(q: $q, country: "US") {
+      items {
+        id
+        volumeInfo {
+          authors
+          averageRating
+          description
+          imageLinks {
+            thumbnail
+          }
+          title
+          subtitle
+          industryIdentifiers {
+            identifier
+            type
+          }
+        }
+      }
+    }
+    openLibrarySearch(q: $q) {
+      docs {
+        author_name
+        title
+        cover_edition_key
+        isbn
+      }
+    }
+  }
+`;
 
 export default function TabOneScreen() {
+  const { data, loading, error } = useQuery(query, {
+    variables: { q: "React Native" },
+  });
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
+      {loading && <ActivityIndicator />}
+      {error && (
+        <>
+          <Text>Error Fetching book</Text>
+          <Text>{error.message}</Text>
+        </>
+      )}
     </View>
   );
 }
@@ -14,16 +56,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
