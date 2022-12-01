e87af60454ea9bda8d0d8b86df2182d855d6bea4
Patrick Gip <gippatrick@gmail.com>
2022-11-30 22:27:19 -0800

    Install and setup Apollo Client #2

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+const API_KEY =
+  "offenbach::stepzen.net+1000::b46ce97b1dceca1f74d5ecbd9a85f67104514efbba5ea1b57edddebc4b32a20b";
+
+const client = new ApolloClient({
+  uri: "https://offenbach.stepzen.net/api/littering-cricket/__graphql",
+  headers: {
+    Authorization: `Apikey ${API_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,8 +25,10 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <ApolloProvider client={client}>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </ApolloProvider>
       </SafeAreaProvider>
     );
   }
